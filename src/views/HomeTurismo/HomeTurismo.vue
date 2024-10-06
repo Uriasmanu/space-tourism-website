7126e95ac299b35c03a1ba649f21ca18baf8ade4
Manoela Urias <maanoelaurias@gmail.com>
2024-10-05 22:09:03 -0300

    finalização do estilo da pagina inicial

--- src/views/HomeTurismo/HomeTurismo.vue
+++ src/views/HomeTurismo/HomeTurismo.vue
@@ -15,9 +15,10 @@
         </header>
 
         <main>
-            <div>
+            <div class="titulo">
+
                 <h2>So, you want to travel to</h2>
-                <h1>Space</h1>
+                <h1>SPACE</h1>
                 <p>Let’s face it; if you want to go to space, you might as well genuinely go to
                     outer space and not hover kind of on the edge of it. Well sit back, and relax
                     because we’ll give you a truly out of this world experience!</p>
