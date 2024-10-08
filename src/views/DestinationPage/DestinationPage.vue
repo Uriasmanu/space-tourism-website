65a94bf806e79b8c9d5db3f0dd969457d05f463c
Manoela Urias <maanoelaurias@gmail.com>
2024-10-07 22:04:26 -0300

    Configuração de exibição da imagem de acordo com a informação clicada

--- src/views/DestinationPage/DestinationPage.vue
+++ src/views/DestinationPage/DestinationPage.vue
@@ -6,20 +6,26 @@
         <main>
             <div class="escolha-imagens">
                 <h2>01 <strong>ESCOLHA SEU DESTINO</strong></h2>
-                <img src="../../assets/destination/image-moon.png" alt="">
+                <img :src="img" :alt="nomeImagem">
             </div>
             <div class="informacoes-luas">
                 <ul>
-                    <li>LUAR</li>
-                    <li>MARTE</li>
-                    <li>EUROPA</li>
-                    <li>TITAN</li>
+                    <li 
+                        @click="selectDestination('moon', 'LUA')"
+                        :class="{ active: img === moonImg }">LUAR</li>
+                    <li 
+                        @click="selectDestination('mars', 'MARTE')"
+                        :class="{ active: img === marsImg }">MARTE</li>
+                    <li 
+                        @click="selectDestination('europa', 'EUROPA')"
+                        :class="{ active: img === europaImg }">EUROPA</li>
+                    <li 
+                        @click="selectDestination('titan', 'TITAN')"
+                        :class="{ active: img === titanImg }">TITAN</li>
                 </ul>
 
-              <!-- <SateliteLua/> -->
-              <!-- <LuaEuropa/> -->
-               <!-- <LuaTitan/> -->
-                <PlanetMarte/>
+                <!-- Componente a ser mostrado baseado na escolha -->
+                <component :is="currentComponent" />
             </div>
         </main>
     </div>
@@ -32,6 +38,12 @@ import PlanetMarte from '@/assets/components/PlanetMarte/PlanetMarte.vue';
 import SateliteLua from '@/assets/components/SateliteLua/SateliteLua.vue';
 import HeaderMenu from '@/assets/shared/Header/HeaderMenu.vue';
 
+// Importe as imagens diretamente
+import moonImg from '@/image/destination/image-moon.png';
+import marsImg from '@/image/destination/image-mars.png';
+import europaImg from '@/image/destination/image-europa.png';
+import titanImg from '@/image/destination/image-titan.png';
+
 export default {
     name: 'Destination',
     components: {
@@ -40,6 +52,51 @@ export default {
         LuaEuropa,
         LuaTitan,
         PlanetMarte,
+    },
+    data() {
+        return {
+            img: moonImg, // Imagem inicial
+            nomeImagem: 'LUA', // Nome da imagem inicial
+            currentComponent: 'SateliteLua', // Componente padrão a ser exibido
+
+            // Imagens
+            moonImg,
+            marsImg,
+            europaImg,
+            titanImg,
+        };
+    },
+   
+    methods: {
+        selectDestination(imageName, nomeImagem) {
+            console.log('Função selectDestination chamada com:', imageName, nomeImagem);
+
+            // Selecionar a imagem correta
+            switch (imageName) {
+                case 'moon':
+                    this.img = this.moonImg;
+                    this.currentComponent = 'SateliteLua';
+                    break;
+                case 'mars':
+                    this.img = this.marsImg;
+                    this.currentComponent = 'PlanetMarte';
+                    break;
+                case 'europa':
+                    this.img = this.europaImg;
+                    this.currentComponent = 'LuaEuropa';
+                    break;
+                case 'titan':
+                    this.img = this.titanImg;
+                    this.currentComponent = 'LuaTitan';
+                    break;
+                default:
+                    this.img = this.moonImg;
+                    this.currentComponent = 'SateliteLua';
+            }
+
+            this.nomeImagem = nomeImagem;
+            console.log('Imagem selecionada:', this.img);
+        }
     }
 }
 </script>
