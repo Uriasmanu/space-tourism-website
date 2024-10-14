1f80c1ad0d299f52af8c707c1eda16c6c133b2e5
Manoela Urias <maanoelaurias@gmail.com>
2024-10-14 11:10:08 -0300

    organização das informações

---
+++ src/assets/components/VeiculoLançamento/VeiculoLançamento.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container-informacoes">
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'VeículoLançamento',
+}
+
+</script>
+
+<style lang="scss" scoped>
+@import'./VeiculoLançamento.scss'
+</style>
--- src/views/TecnologiaView/TecnologiaView.vue
+++ src/views/TecnologiaView/TecnologiaView.vue
@@ -1,15 +1,43 @@
 <template>
+    <div class="containe-tecnologia">
+        <header>
+            <HeaderMenu />
+            <div class="menu">
+                <MenuMobile />
+            </div>
+        </header>
+        <main>
+            <div class="titulo">
+                <h2>03 <strong>SPACE LAUNCH 101</strong></h2>
 
+                <div class="informacoes">
+                    <div class="botao-informacoes">
+                        <input type="checkbox" id="valor">
+                        <label for="valor">1</label>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
 </template>
 
 <script>
+import VeiculoLançamento from '@/assets/components/VeiculoLançamento/VeiculoLançamento.vue';
+import HeaderMenu from '@/assets/shared/Header/HeaderMenu.vue';
+import MenuMobile from '@/assets/shared/MenuMobile/MenuMobile.vue';
 
-    export default{
-        name: 'TecnologiaView',
+export default {
+    name: 'TecnologiaView',
+    components: {
+        HeaderMenu,
+        MenuMobile,
+        VeiculoLançamento,
+
     }
+}
 
 </script>
 
 <style lang="scss" scoped>
-@import './TecnologiaView.scss'
+@import './_TecnologiaView.scss'
 </style>
